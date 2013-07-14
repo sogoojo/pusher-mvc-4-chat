4881e8be3e1d2033c09dfed4064cc0afc790e995
Oluwasogo Ojo <sogoojo@gmail.com>
2013-07-14 19:20:56 +0100

    MVC Chat app
    A little chat application written in C# using MVC

---
+++ pusher-mvc-4-chat/Geekbeing.Pusher.LiveChat.Web/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Configuration;
+using System.Web.Mvc;
+using PusherRESTDotNet;
+
+namespace Geekbeing.Pusher.LiveChat.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static readonly PusherProvider Provider = new PusherProvider
+         (
+             ConfigurationManager.AppSettings["pusher_app_id"],
+             ConfigurationManager.AppSettings["pusher_key"],
+             ConfigurationManager.AppSettings["pusher_secret"]
+         );
+
+        public ActionResult Index(string chatMessage, string username)
+        {
+            var now = DateTime.UtcNow;
+            var request = new ObjectPusherRequest(
+                "chat_channel",
+                "message_received",
+                new
+                {
+                    message = chatMessage,
+                    user = username,
+                    timestamp = now.ToShortDateString() + " " + now.ToShortTimeString()
+                });
+            Provider.Trigger(request);
+            return View();
+        }
+    }
+}
---
+++ pusher-mvc-4-chat/Geekbeing.Pusher.LiveChat.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Geekbeing.Pusher.LiveChat.Web.MvcApplication" Language="C#" %>
